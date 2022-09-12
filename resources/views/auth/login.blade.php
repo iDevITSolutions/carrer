976216c924269e03634f03e89e2b82f51517d561
gronel <ronelgonzales5@gmail.com>
2022-09-12 13:55:42 +0800

    initial push

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Login') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+
+                            <div class="form-group row">
+                                <div class="col-md-6 offset-md-3">
+                                    <a href="{{ route('login.google') }}" class="btn btn-danger btn-block">Login via
+                                        Google</a>
+
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
